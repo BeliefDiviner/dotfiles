d00d1e04e10842d8a5be726d448afc75361eb91d
BeliefDiviner <41856345+BeliefDiviner@users.noreply.github.com>
2024-02-04 04:25:00 +0600

    feat: Add wsl clipboard interaction functionality.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,6 +2,22 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.o.clipboard = 'unnamedplus'
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
+vim.o.scrolloff = 12
 
 vim.o.number = true
 vim.o.relativenumber = true
@@ -9,11 +25,14 @@ vim.o.relativenumber = true
 vim.o.signcolum = 'yes'
 
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
 
 vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 
--- vim.o.mouse = 'a'
+vim.o.mouse = 'a'
 
