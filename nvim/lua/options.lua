29c4f4164f7e704408f35e4d1deb3350f5c28d03
BeliefDiviner <41856345+BeliefDiviner@users.noreply.github.com>
2024-02-13 14:08:52 +0600

    chore: Apply formatter.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,20 +1,20 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.o.clipboard = 'unnamedplus'
-if vim.fn.has('wsl') == 1 then
-    vim.g.clipboard = {
-        name = 'WslClipboard',
-        copy = {
-            ['+'] = 'clip.exe',
-            ['*'] = 'clip.exe',
-        },
-        paste = {
-            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        },
-        cache_enabled = 0,
-    }
+vim.o.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
 end
 
 vim.o.scrolloff = 12
@@ -22,7 +22,7 @@ vim.o.scrolloff = 12
 vim.o.number = true
 vim.o.relativenumber = true
 
-vim.o.signcolum = 'yes'
+vim.o.signcolum = "yes"
 
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
@@ -34,6 +34,6 @@ vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-N>", { silent = true })
