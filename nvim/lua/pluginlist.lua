c47503a9c5947d28e6415f74501cbf5b99d9e889
BeliefDiviner <41856345+BeliefDiviner@users.noreply.github.com>
2024-02-21 02:44:40 +0600

    feat: Made path relative in lualine.

--- nvim/lua/pluginlist.lua
+++ nvim/lua/pluginlist.lua
@@ -1,79 +1,87 @@
 return {
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd("colorscheme gruvbox")
-        end
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                icons_enabled = true,
-                theme = 'gruvbox',
-            })
-        end,
-    },
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "gruvbox",
+				},
+				sections = {
+					lualine_c = { {
+						"filename",
+						path = 1,
+					} },
+				},
+			})
+		end,
+	},
 
-    -- Language Server Protocol config helpers.
-    "neovim/nvim-lspconfig",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "mfussenegger/nvim-lint",
-    {
-        "stevearc/conform.nvim",
-        opts = {},
-    },
+	-- Language Server Protocol config helpers.
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"mfussenegger/nvim-lint",
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
 
-    -- NeoVim environement helpers.
-    "folke/neodev.nvim",
+	-- NeoVim environement helpers.
+	"folke/neodev.nvim",
 
-    -- Code completion.
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
+	-- Code completion.
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
 
-            "hrsh7th/cmp-nvim-lsp",
-        },
-    },
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
 
-    -- Code highlighting.
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ':TSUpdate',
-    },
+	-- Code highlighting.
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 
-    -- Telescope searcher and fuzzy search.
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = 'make',
-    },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim",
-        }
-    },
+	-- Telescope searcher and fuzzy search.
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-    -- Git support.
-    "lewis6991/gitsigns.nvim",
-    "sindrets/diffview.nvim",
+	-- Git support.
+	"lewis6991/gitsigns.nvim",
+	"sindrets/diffview.nvim",
 }
