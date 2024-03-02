154416a6e96e527d4f7eff79ad5f5f877244bb9f
BeliefDiviner <41856345+BeliefDiviner@users.noreply.github.com>
2024-03-02 22:01:06 +0500

    feat: trouble keybinds

--- nvim/lua/user-plugins/trouble.lua
+++ nvim/lua/user-plugins/trouble.lua
@@ -2,4 +2,36 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
+	keys = {
+		{
+			"<leader>tt",
+			function()
+				require("trouble").toggle()
+			end,
+		},
+		{
+			"<leader>tw",
+			function()
+				require("trouble").toggle("workspace_diagnostics")
+			end,
+		},
+		{
+			"<leader>td",
+			function()
+				require("trouble").toggle("document_diagnostics")
+			end,
+		},
+		{
+			"<leader>tq",
+			function()
+				require("trouble").toggle("quickfix")
+			end,
+		},
+		{
+			"<leader>tl",
+			function()
+				require("trouble").toggle("loclist")
+			end,
+		},
+	},
 }
